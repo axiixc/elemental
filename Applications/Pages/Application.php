1934547a665b11c4cc12dc6a91f2285106d0ce99
AXIIXC <axiixc@Carbon.local>
2009-07-23 19:40:19 -0400

    Yay updates to Pages app

--- Applications/Pages/Application.php
+++ Applications/Pages/Application.php
@@ -1,11 +1,18 @@
 <?php # Pages Application
 
-if (is_string($_GET['arg']) and strlen($_GET['arg']) == 32) // View specific page
+if (!is_null($_GET['arg']) and $_GET['arg'] != '') // View specific page
 {
    exMethod("Pages: View -> Single Page with ID {$_GET['arg']}");
    $page = new Page(mysql_safe($_GET['arg']), true);
    
-   add(template('Single Page', 'Page Name: %1$s<br />Date Mod: %3$s | Date Created: %4$s<br />Author: %7$s<br /><pre style="color:green">%2$s</pre>'), $page->name, $page->content, format_date($page->date_modified), format_date($page->date_created), $page->author->id, $page->author->username, $page->author->display_name);
+   if (!is_null($page->id))
+   {
+      add(template('Single Page', 'Page Name: %1$s<br />Date Mod: %3$s | Date Created: %4$s<br />Author: %7$s<br /><pre style="color:green">%2$s</pre>'), $page->name, $page->content, format_date($page->date_modified), format_date($page->date_created), $page->author->id, $page->author->username, $page->author->display_name);
+   }
+   else
+   {
+      error('Page Not Found', 'You entered an invalid page.');
+   }
 }
 else // View all pages
 {
--- Applications/Pages/Resources.php
+++ Applications/Pages/Resources.php
@@ -3,27 +3,60 @@
 class SharedPage
 {
    
-   static public $statements;
+   static public $statements, $identifiers;
    
    private function __construct() {}
    
    public static function Initialize()
    {
+      self::$statements['fetch_all_not_draft_ids'] = "SELECT `id`, `ref_id`, `name` FROM `[prefix]pages` WHERE `draft` != 1 ORDER BY `date_created` ASC";
+      self::$statements['fetch_all_ids'] = "SELECT `id`, `ref_id`, `name` FROM `[prefix]pages` ORDER BY `date_created` ASC";
       self::$statements['fetch_with_id'] = "SELECT *  FROM `[prefix]pages` WHERE `id` LIKE CONVERT(_utf8 '%s' USING latin1) COLLATE latin1_swedish_ci";
-      // self::$statements['fetch_with_data'] = "";
+      self::$statements['fetch_with_data'] = "SELECT * FROM `[prefix]pages` WHERE `%s` LIKE CONVERT(_utf8 '%s' USING latin1) COLLATE latin1_swedish_ci";
       self::$statements['update_with_id'] = "UPDATE `[database]`.`[prefix]pages` SET `%s` = '%s' WHERE CONVERT(`[prefix]pages`.`id` USING utf8) = '%s' LIMIT 1;";
       // self::$statements['update_with_data'] = "";
-      self::$statements['create'] = "INSERT INTO `[database]`.`[prefix]pages` (`id`, `name`, `content`, `author`, `date_modified`, `date_created`, `draft`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');";
+      self::$statements['create'] = "INSERT INTO `[database]`.`[prefix]pages` (`id`, `name`, `ref_name`, `content`, `author`, `date_modified`, `date_created`, `draft`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');";
       self::$statements['delete'] = "DELETE FROM `[prefix]pages` WHERE CONVERT(`[prefix]pages`.`id` USING utf8) = '%s' LIMIT 1";
       // self::$statements['update'] = "";
-      self::$statements['is_page'] = "SELECT `id` FROM `[prefix]pages` WHERE `id` LIKE CONVERT(_utf8 '%s' USING latin1) COLLATE latin1_swedish_ci";
+      self::$statements['is_page'] = "SELECT `id`, `ref_id` FROM `[prefix]pages` WHERE `id` LIKE CONVERT(_utf8 '%s' USING latin1) COLLATE latin1_swedish_ci";
       self::$statements['is_draft'] = "SELECT `draft` FROM `[prefix]pages` WHERE `id` LIKE CONVERT(_utf8 '%s' USING latin1) COLLATE latin1_swedish_ci";
+      
+      $_identifiers = query(self::$statements['fetch_all_ids']);
+      if (is_resource($_identifiers))
+      {
+         if (mysql_num_rows($_identifiers) > 0)
+         {
+            while ($page = mysql_fetch_assoc($_identifiers))
+            {
+               self::$identifiers[] = array(
+                  'id' => $page['id'], 
+                  'ref_id' => $page['ref_id'], 
+                  'name' => $page['name'], 
+                  'ref_name' => $page['ref_name']
+               );
+            }
+         }
+         else
+         {
+            exLog('SharedPage->Initialize(): No pages, initializing with no identifiers.');
+         }
+      }
+      else
+      {
+         exLog('SharedPage->Initialize(): Error fetching page identifiers.');
+      }
+         
    }
    
    public static function Fetch($offset = 0, $count = 30, $full = false, $drafts = false)
    {
-      // (full) ? return new Page() objects : return page ids ;
-      // (drafts) ? include drafts : do not include drafts ;
+      $data_source = ($drafts) ? $identifiers_all : $identifiers ;
+      
+      for ($i=$offset; $i < $count; $i++) { 
+         $output[] = ($full) ? new Page($data_source[$i]['id']) : $data_source[$i]['id'] ;
+      }
+      
+      return $output;
    }
    
 }
@@ -37,15 +70,36 @@ class Page
    public $id, $name, $content, $author = array(), $date_modified, $date_created, $draft;
    
    public function __construct($page_id, $read_only = true)
-   {  
+   {   
+      exMethod("Page: Initializing with input($page_id)");
       $this->read_only = $read_only;
       
+      /* The "try to fetch" Block */
+      // Try with Identifier
+      exMethod("Page: Trying 'fetch with id'");
       $_page = query(SharedPage::$statements['fetch_with_id'], $page_id);
-      if (is_resource($_page) and mysql_num_rows($_page) > 0)
+      
+      // Try with Reference Identifier
+      if (mysql_num_rows($_page) != 1)
+      {
+         exMethod("Page: Trying 'fetch with ref'");
+         $_page = query(SharedPage::$statements['fetch_with_data'], 'ref_id', $page_id);
+      }
+      
+      // Try with crunch(Name)
+      if (mysql_num_rows($_page) != 1)
+      {
+         exMethod("Page: Trying 'fetch with name'");
+         $_page = query(SharedPage::$statements['fetch_with_data'], 'ref_name', crunch($page_id, true));
+      }
+      
+      if (is_resource($_page) and mysql_num_rows($_page) == 1)
       {
          $page = mysql_fetch_assoc($_page);
          $this->id = $page['id'];
+         $this->ref_id = $page['ref_id'];
          $this->name = $page['name'];
+         $this->ref_name = $page['ref_name'];
          
          // TODO add ex: parsing, add special comment parsing, add [[]] parsing
          $this->content = $page['content'];
@@ -58,7 +112,7 @@ class Page
       }
       else
       {
-         exLog("Page->__construct(): No page at id $page_id");
+         exLog("Page->__construct(): No page exists with ID, Ref ID, or name of $page_id.");
       }
    }
    
@@ -184,6 +238,7 @@ class BlankPage extends Page
 
       $this->$id = appPagesGenerateKey();
       $this->name = $args['name'];
+      $this->ref_name = crunch($args['name'], true);
       $this->content = $args['content'];
    
       if (is_string($args['author']) and strlen($args['author'] == 32))
@@ -202,7 +257,14 @@ class BlankPage extends Page
       // Is this needed?
       $draft = ($this->draft) ? '1' : '0';
       
-      query(SharedPage::$statements['create'], $this->id, $this->name, $this->content, $this->author->id, $this->date_modified, $this->date_created, $draft);
+      query(SharedPage::$statements['create'], $this->id, $this->name, $this->ref_name, $this->content, $this->author->id, $this->date_modified, $this->date_created, $draft);
+      
+      $__this = query(SharedPage::$statements['is_page'], $this->id);
+      if (is_resource($__this) and mysql_num_rows($__this) > 0)
+      {
+         $_this = mysql_fetch_assoc($__this);
+         $this->ref_id = $_this['ref_id'];
+      }
    }
    
    /* __set() is handled by inheritance ;) */
@@ -222,11 +284,20 @@ function appPagesGenerateKey()
 
 function appPagesIsPage($id)
 {
-   $_page = query(SharedPage::$statements['is_page'], $id);
-   return (is_resource($_page) and mysql_num_rows($_key) == 1);
+   $_page = query(SharedPage::$statements['is_page'], mysql_safe($id));
+   return (is_resource($_page) and mysql_num_rows($_key) == 0);
 }
 
 function appPagesIsDraft($id)
 {
-   
+   if (appPagesIsPage($id))
+   {
+      $_page = query(SharedPage::$statements['is_draft'], mysql_safe($id));
+      return (is_resource($_page) and mysql_num_rows($_page) == 1);
+   }
+   else
+   {
+      exLog("appPagesIsDraft(): Invalid reading, $id is not a valid page.");
+      return false;
+   }
 }
